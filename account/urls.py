26cd29b86c4ef731011cb184b97870ea72fada60
mohammadjh <mohammadjh@callnsolution.com>
2017-04-11 17:06:09 +0600

    Added url and view for changing password

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
-from . import views
+from django.contrib.auth import views
+from .views import dashboard
 
 urlpatterns = [
     # post views
-    url(r'^login/$', views.user_login, name='login'),
+    # url(r'^login/$', views.user_login, name='login'),
+    url(r'^$', dashboard, name='dashboard'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^password_change/$', views.password_change, name='password_change'),
+    url(r'^password_change/done/$', views.password_change_done, name='password_change_done'),
 ]
