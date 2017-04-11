0ea7d1f63e4ae2f266cf41418e20aa37086e12a0
mohammadjh <mohammadjh@callnsolution.com>
2017-04-11 19:40:43 +0600

    Added urls and views for user registration

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.contrib.auth import views
-from .views import dashboard
+from .views import dashboard, register
 
 urlpatterns = [
     # post views
     # url(r'^login/$', views.user_login, name='login'),
     url(r'^$', dashboard, name='dashboard'),
+    url(r'^register/$', register, name='register'),
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
     url(r'^password_change/$', views.password_change, name='password_change'),
